2dbc4ac1800aa492a8f0af627246a62db23e0077
allisonbday <allisonbday@byui.edu>
2022-03-29 17:57:05 -0600

    added map & top

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+locations = pd.read_csv("locations.csv")
+
+
+top = st.container()
+
+with top:
+    st.title("Subway & McDonalds Analysis")
+
+    st.write("Here are all of the Subway and McDonalds locations 4 states: Utah, Colorado, Idaho, and Wyoming")
+    st.map(data=locations, zoom=None, use_container_width=True)
+
